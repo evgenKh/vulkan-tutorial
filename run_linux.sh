f013875be64066efd40f1359ce6f3f43704be1ce
Yevhenii Kharchenko <evgen.kharchenko.ua@gmail.com>
2020-06-15 19:07:10 +0300

    'Base code' in Vs Code

---
+++ run_linux.sh
@@ -0,0 +1,4 @@
+export VULKAN_SDK=/home/kharj/Projects/Onboarding/vulkan-sdk/x86_64/
+export LD_LIBRARY_PATH=$VULKAN_SDK/lib
+export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d
+linux/Tutorial
